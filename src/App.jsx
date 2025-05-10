0626bc1ac3c62b5f4971441fc710373d5813007a
gorayafa <faham.goraya@mail.utoronto.ca>
2025-05-09 22:21:56 -0400

    alr

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const y = 5
+   
+
+  return (
+    <>
+    <h1> </h1>
+    
+    </>
+  )
+}
+
+export default App
