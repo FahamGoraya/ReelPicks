65c01b0e1f89ec1dab04092fc968b89935933749
gorayafa <faham.goraya@mail.utoronto.ca>
2025-05-14 17:45:32 -0400

    completed good amount of front end

--- backend/movies.js
+++ backend/movies.js
@@ -8,43 +8,94 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocoldssdsfafsfasf",
-    important: true
-  }
-]
+  app.get('/api/movies/trend',(request,responese) =>{
+    const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRmMzc3MDFjNzc4MTMwMzU1OTFiMGFkY2VkMDk4YSIsIm5iZiI6MTc0NjYyOTU0MC40Niwic3ViIjoiNjgxYjczYTQwOTEwNmVmMTgxZWYwZGZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oWmCKq0ehc0nTjSth_F9Z8-rvSOKdUJmXDuLswSZuHo'
+      }
+    };
 
-  app.get('/api/persons',(request,response)=>{
-    response.json(notes)
-  }
-  )
+  fetch(url, options)
+    .then(res => res.json())
+    .then(json => responese.json(json))
+    .catch(err => console.error(err));
+  } )
 
-  app.get('/api/persons/:id',(request,response)=>{
-    const id = request.params.id
-    const temp = notes.filter(n=>n.id===id)
-    if(temp.length>0){
-      response.json(temp)
-    }
-    else{
-      response.status(404).end()
-    }
+    app.get('/api/movies/top',(request,responese) =>{
+      const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRmMzc3MDFjNzc4MTMwMzU1OTFiMGFkY2VkMDk4YSIsIm5iZiI6MTc0NjYyOTU0MC40Niwic3ViIjoiNjgxYjczYTQwOTEwNmVmMTgxZWYwZGZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oWmCKq0ehc0nTjSth_F9Z8-rvSOKdUJmXDuLswSZuHo'
+        }
+      };
 
-  })
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => responese.json(json))
+        .catch(err => console.error(err));
+
+
+    } )
+
+    app.get('/api/movies/coming',(request,responese) =>{
+      const url = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1';
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRmMzc3MDFjNzc4MTMwMzU1OTFiMGFkY2VkMDk4YSIsIm5iZiI6MTc0NjYyOTU0MC40Niwic3ViIjoiNjgxYjczYTQwOTEwNmVmMTgxZWYwZGZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oWmCKq0ehc0nTjSth_F9Z8-rvSOKdUJmXDuLswSZuHo'
+        }
+      };
+
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => responese.json(json))
+        .catch(err => console.error(err));
+    } )
+
+        app.get('/api/movies/:id/info',(request,responese) =>{
+        const id = request.params.id
+
+        const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+        const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRmMzc3MDFjNzc4MTMwMzU1OTFiMGFkY2VkMDk4YSIsIm5iZiI6MTc0NjYyOTU0MC40Niwic3ViIjoiNjgxYjczYTQwOTEwNmVmMTgxZWYwZGZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oWmCKq0ehc0nTjSth_F9Z8-rvSOKdUJmXDuLswSZuHo'
+        }
+      };
+
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => responese.json(json))
+        .catch(err => console.error(err));
+    } )
+
+
+      app.get('/api/movies/:id/trailer',(request,responese) =>{
+        const id = request.params.id
+
+        const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+        const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMmRmMzc3MDFjNzc4MTMwMzU1OTFiMGFkY2VkMDk4YSIsIm5iZiI6MTc0NjYyOTU0MC40Niwic3ViIjoiNjgxYjczYTQwOTEwNmVmMTgxZWYwZGZmIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.oWmCKq0ehc0nTjSth_F9Z8-rvSOKdUJmXDuLswSZuHo'
+        }
+      };
+
+      fetch(url, options)
+        .then(res => res.json())
+        .then(json => responese.json(json))
+        .catch(err => console.error(err));
+    } )
 
   
-
 
   const PORT = 3001
   app.listen(PORT, () => {
