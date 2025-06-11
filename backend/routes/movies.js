ec20c1a4d2885628bf8df15280a91876769bc7c8
gorayafa <faham.goraya@mail.utoronto.ca>
2025-06-11 15:11:48 -0400

    Done cleaning up backend

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-
-// Reuse API key from environment
+const {
+  getTrend,
+  getByIdImg,
+  getComing,
+  getTop,
+  getByIdInfo,
+  getByIdVid,
+} = require("../controller/movieController");
 
 const getOptions = () => ({
   method: "GET",
@@ -11,64 +17,11 @@ const getOptions = () => ({
     Authorization: `Bearer ${process.env.TMBD_API_KEY}`,
   },
 });
-
-router.get("/trend", (req, res) => {
-  const url = "https://api.themoviedb.org/3/trending/all/day?language=en-US";
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/top", (req, res) => {
-  const url =
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/coming", (req, res) => {
-  const url =
-    "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/:id/info", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/:id/img", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/movie/${id}/images`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/:id/vid", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
+router.get("/trend", getTrend);
+router.get("/top", getTop);
+router.get("/coming", getComing);
+router.get("/:id/info", getByIdInfo);
+router.get("/:id/img", getByIdImg);
+router.get("/:id/vid", getByIdVid);
 
 module.exports = router;
--- backend/routes/tv.js
+++ backend/routes/tv.js
@@ -1,44 +1,13 @@
-require("dotenv").config();
 const express = require("express");
 const router = express.Router();
+const {
+  getTvidImg,
+  getTvidVig,
+  getTvidInfo,
+} = require("../controller/tvController");
 
-// Reusable fetch options
-const getOptions = () => ({
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${process.env.TMBD_API_KEY}`,
-  },
-});
-
-router.get("/:id/info", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/tv/${id}?language=en-US`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/:id/img", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/tv/${id}/images`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
-
-router.get("/:id/vid", (req, res) => {
-  const id = req.params.id;
-  const url = `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`;
-
-  fetch(url, getOptions())
-    .then((r) => r.json())
-    .then((data) => res.json(data))
-    .catch((err) => console.error(err));
-});
+router.get("/:id/info", getTvidInfo);
+router.get("/:id/img", getTvidImg);
+router.get("/:id/vid", getTvidVig);
 
 module.exports = router;
